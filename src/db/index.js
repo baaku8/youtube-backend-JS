3eaf15537d0e26699c7ebf6124fa82db5b876f21
Piyush Chatterjee <capsicum.broth@gmail.com>
2026-03-09 20:24:36 +0530

    Update .gitignore, add user controller, and enhance database connection handling

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+//whenever we are connecting database,we need to keep these 2 things in mind:
+//there might be some problems in connecting the database, so we always enclose them with try-catch.
+//database is assumed to be in another continent, so it's always recommended to use async-await to make the code asynchronous.
+
 const connectDB= async ()=>{
      try {
         const connectionInstance= await mongoose.connect(`${process.env.MONGO_DB}/${DB_NAME}`)
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import dotenv from 'dotenv'         //kuch bhi kaam start krne se pehle env variables load krlena.
-dotenv.config({ path: './.env' })   
-import connectDB from "./db/index.js"; 
-import { app } from './app.js';  
+dotenv.config({ path: './.env' })
+import connectDB from "./db/index.js";
+import { app } from './app.js';
 // require('dotenv').config() 
 connectDB()
-.then(()=>{
-    app.on("error",(error)=>{
-       console.log("ERORR: ",error);
-       throw error;
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERORR: ", error);
+            throw error;
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running in https://localhost:${process.env.PORT}`);
+        })
     })
-    app.listen(process.env.PORT,()=>{
-        console.log(`Server is running in https://localhost:${process.env.PORT}`);
+    .catch((error) => {
+        console.log("MongoDB connection Failure")
+        throw error;
     })
-})
-.catch((error)=>{
-    console.log("MongoDB connection Failure")
-    throw error;
-})
-;          //database connected
+    ;          //database connected
+
+
+
